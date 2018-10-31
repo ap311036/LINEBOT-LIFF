77e4f64627f75264bccaa5f582d369218ccfd4e9
S <ap311036@gmail.com>
2018-10-31 12:45:08 +0800

    fix py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-# 変更
-# app = Flask(__name__)
-app = Flask(__name__, static_folder='imgs')
+from flask import Flask, request, render_template, make_response
+from flask_bootstrap import Bootstrap
 
+import os
+import uuid
+import base64
+
+from PIL import Image
+import warnings
+warnings.simplefilter('error', Image.DecompressionBombWarning)
+
+app = Flask(__name__, static_folder='imgs')
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def do_get():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
 
 # 追加
 @app.route('/saveimage', methods=['POST'])
