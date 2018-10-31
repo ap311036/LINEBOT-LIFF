40608d833c699026e549c9d19e73d45ccbd26730
S <ap311036@gmail.com>
2018-10-31 21:15:06 +0800

    commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, make_response
 from flask_bootstrap import Bootstrap
-from io import BytesIO
+
 import os
 import uuid
 import base64
@@ -33,15 +33,15 @@ def saveimage():
     # Saving image in the 'imgs' folder temporarily. Should be deleted after a certain period of time
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
-    with open(os.path.join(dir_name, '{}.png'.format(img_name)), 'wb') as img:
-        img.write(BytesIO(base64.b64decode(event['image'].split(",")[1])))
+    with open(os.path.join(dir_name, '{}.jpg'.format(img_name)), 'wb') as img:
+        img.write(base64.b64decode(event['image'].split(",")[1]))
 
-    original = Image.open(os.path.join(dir_name, '{}.png'.format(img_name)))
+    original = Image.open(os.path.join(dir_name, '{}.jpg'.format(img_name)))
     # Needs simple validation of format for security since Pillow supports various type of Images
-    if(original.format != 'PNG'):
+    if(original.format != 'JPEG'):
         return make_response('Unsupported image type.', 400)
 
     original.thumbnail((240, 240), Image.ANTIALIAS)
-    original.save(os.path.join(dir_name, '{}_240.png'.format(img_name)), 'PNG')
+    original.save(os.path.join(dir_name, '{}_240.jpg'.format(img_name)), 'JPEG')
 
-    return make_response(img_name, 200)
+    return make_response(img_name, 200,original.format)
